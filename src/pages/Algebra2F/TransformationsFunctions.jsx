4e7fa7944d943ec8a25035353ec48934db65b8c5
Teacher <teacher@FMBA5004.home>
2022-10-18 18:37:14 -1000

    update

--- src/pages/Algebra2F/TransformationsFunctions.jsx
+++ src/pages/Algebra2F/TransformationsFunctions.jsx
@@ -5,7 +5,6 @@ import "../Product.css"
 import {Link} from 'react-router-dom'
 import { ParentFunctions, ParentFunctions2, TransformationsofGraphs, TransformationsReview, TransformationsWorksheet3, TransformationWorksheet2, TransformingFunctionsWorksheet } from "./Preview";
 // import {ParentFunctions,TransformationsofGraphs,TransformationsReview,ParentFunctions2,TransformationWorksheet2,TransformationsWorksheet3,TransformingFunctionsWorksheet} from "./Preview"
-
 // import {ParentFunctions,ParentFunctions2,TransformationWorksheet2,TransformationsofGraphs,TransformationsReview,TransformationsWorksheet3, TransformingFunctionsWorksheet} from './Preview'
 
 const TransformationsFunctions = () => (
@@ -30,10 +29,10 @@ const TransformationsFunctions = () => (
 
 
         <File title='Parent Functions' img={ParentFunctions} link='https://drive.google.com/file/d/13s_qbsT7nE0A5JFs7k9v129EkJpNBKa8/view?usp=sharing' target='_blank' body = 'Download PDF' />
-        <File title='Parent Functions' img={ParentFunctions2} link='https://drive.google.com/file/d/15wGGUBpyi7A6VZbjq17FcH82lQfs5FWD/view?usp=sharing' target='_blank' body = 'Download PDF' />
-        <File title='Parent Functions' img={TransformationsWorksheet3} link='https://drive.google.com/file/d/104d6g-TOsSCVob3-HbdDBxcKQrD9x8Kd/view?usp=sharing' target='_blank' body = 'Download PDF' />
-        <File title='Parent Functions' img={TransformationWorksheet2} link='https://drive.google.com/file/d/1xX-vS8i-eJOMzv3oR1g-OuUCAAvN2RwO/view?usp=sharing' target='_blank' body = 'Download PDF' />
-        <File title='Parent Functions' img={TransformingFunctionsWorksheet} link='https://drive.google.com/file/d/1Jq1PhyBDT6r7RT1_pPyM4xTKZ0RWp-au/view?usp=sharing' target='_blank' body = 'Download PDF' />
+        <File title='Parent Functions 2' img={ParentFunctions2} link='https://drive.google.com/file/d/15wGGUBpyi7A6VZbjq17FcH82lQfs5FWD/view?usp=sharing' target='_blank' body = 'Download PDF' />
+        <File title='Transformations Worksheet 3' img={TransformationsWorksheet3} link='https://drive.google.com/file/d/104d6g-TOsSCVob3-HbdDBxcKQrD9x8Kd/view?usp=sharing' target='_blank' body = 'Download PDF' />
+        <File title='Transformations Worksheet 2' img={TransformationWorksheet2} link='https://drive.google.com/file/d/1xX-vS8i-eJOMzv3oR1g-OuUCAAvN2RwO/view?usp=sharing' target='_blank' body = 'Download PDF' />
+        <File title='Transformations Worksheet' img={TransformingFunctionsWorksheet} link='https://drive.google.com/file/d/1Jq1PhyBDT6r7RT1_pPyM4xTKZ0RWp-au/view?usp=sharing' target='_blank' body = 'Download PDF' />
 
 <File title='Transformations of Graphs' img={TransformationsofGraphs} link='https://drive.google.com/file/d/1XhTAKRgA1MHNAIf9dXsHUl8E9kM1iV1c/view?usp=sharing' target='_blank' body = 'Download PDF' />
 <File title='Transformations Review' img={TransformationsReview} link='https://drive.google.com/file/d/1RCE5A6sw41pHfdSIZOi2YwnXdV2-gmhB/view?usp=sharing' target='_blank' body = 'Download PDF' />
